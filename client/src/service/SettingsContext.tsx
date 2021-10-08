06120ea0d71cdccd0c3d3168d04e3a470d3fc922
Aditya Kumar <80571997+AdityaStark7@users.noreply.github.com>
2021-10-09 01:25:31 +0530

    added settings context

---
+++ client/src/service/SettingsContext.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { SettingsContextType } from "types";
+import { socket } from "service/socket";
+export const SettingContext = React.createContext<SettingsContextType | null>(null);
+
+const SettingsProvider: React.FC<React.ReactNode> = ({ children }) => {
+  const [language, setLanguage] = React.useState<string>("text/x-c++src");
+  const [fontSize, setFontSize] = React.useState<number>(18);
+  const [theme, setTheme] = React.useState<string>("monokai");
+  const [keybinds, setKeybinds] = React.useState<string>("sublime");
+
+  const handleLanguageChange = (value: string, id: string, broadcast: boolean) => {
+    setLanguage(value);
+    if (broadcast) socket.emit("programming-language", { data: value, roomID: id });
+  };
+
+  const handleThemeChange = (value: string) => {
+    setTheme(value);
+  };
+
+  const handleFontSizeChange = (value: number) => {
+    setFontSize(value);
+  };
+
+  const handleKeybindsChange = (value: string) => {
+    setKeybinds(value);
+  };
+
+  return (
+    <SettingContext.Provider
+      value={{
+        language,
+        fontSize,
+        theme,
+        keybinds,
+        handleLanguageChange,
+        handleFontSizeChange,
+        handleThemeChange,
+        handleKeybindsChange,
+      }}
+    >
+      {children}
+    </SettingContext.Provider>
+  );
+};
+
+export default SettingsProvider;
